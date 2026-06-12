7f0f835b5bd37b776cf2205280e191a10a26a100
moisegit528 <moisesgithub@gmail.com>
2026-06-11 23:44:28 -0300

    Spring-Boot / Spring Security (Prática)

--- src/main/java/com/moisegit/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/moisegit/todolist/filter/FilterTaskAuth.java
@@ -7,12 +7,35 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
 public class FilterTaskAuth  extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // pegar a autenticação (usuário e senha)
+        var authorization = request.getHeader("Authorization");
+
+        String authString = new String(
+                Base64.getDecoder().decode(authorization.replace("Basic ", ""))
+        );
+
+        // ["Usuario:" + username > moisesspring]
+        // ["Password:" + password > 12345]
+        /*String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+        System.out.println("AUTHORIZATION");
+        System.out.println("Usuário: " + username);
+        System.out.println("Password: " + password);
+*/
+        // validar usuário
+
+        // validar senha
+
+        // segue viagem
 
         filterChain.doFilter(request,response);
     }
