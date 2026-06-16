e183455b339e4e67478f9402a62d8a0dffdd7c68
moisegit528 <moisesgithub@gmail.com>
2026-06-15 22:41:24 -0300

    Spring-Boot (validação de datas)

--- src/main/java/com/moisegit/todolist/task/ITaskController.java
+++ src/main/java/com/moisegit/todolist/task/ITaskController.java
@@ -1,11 +1,17 @@
 package com.moisegit.todolist.task;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController // Usado para criar APIs - Retorna dados direto no corpo da resposta (json/xml)
 @RequestMapping("/tasks") // Responsável por informar a estrutura da rota http
@@ -15,9 +21,17 @@ public class ITaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {// TaskModel(entidade), create(função), taskModel(variável)
-        System.out.println("Chegou no Controller");
-       var task =  this.taskRepository.save(taskModel);
-       return task;
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {// TaskModel(entidade), create(função), taskModel(variável)
+       var idUser = request.getAttribute("idUser");
+       taskModel.setIdUser((UUID) idUser);
+
+       var currentDate = LocalDateTime.now();
+       if(currentDate.isAfter(taskModel.getStartTime()) || currentDate.isAfter(taskModel.getEndTime())) {
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de ínicio / data de término deve ser maior do que a data atual");
+       }
+
+       var task = this.taskRepository.save(taskModel);
+       return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
+
