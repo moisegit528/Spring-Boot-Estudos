cfe955ed6eab7a09ff542a22bc768990093b894e
moisegit528 <moisesgithub@gmail.com>
2026-06-18 22:00:18 -0300

    Update validação de data/hora. Add funcionalidade para listar tarefas com base na autenticação

--- src/main/java/com/moisegit/todolist/task/TaskRepository.java
+++ src/main/java/com/moisegit/todolist/task/TaskRepository.java
@@ -2,7 +2,9 @@ package com.moisegit.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByidUser(UUID idUser); // lista do taskModel, baseado no usuário "idUser"
 }
